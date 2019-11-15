0e030c8560629826af2cf59299aa7212d9c19dbd
Romulo Rocha <romulorocha063@gmail.com>
2019-11-15 08:45:10 -0300

    mock axios

--- __tests__/store/sagas/Techs.test.js
+++ __tests__/store/sagas/Techs.test.js
@@ -1,13 +1,29 @@
 import { runSaga } from 'redux-saga';
-import { getTechsSuccess } from '~/store/techs/actions';
+
+import MockAdapter from 'axios-mock-adapter';
+import api from '~/services/api';
+
+import { getTechsSuccess, getTechsFailure } from '~/store/techs/actions';
 import { getTechs } from '~/store/techs/sagas';
+
+const apiMock = new MockAdapter(api);
 
 describe('Techs saga', () => {
   it('should be able to fetch techs', async () => {
     const dispatch = jest.fn();
 
+    apiMock.onGet().reply(200, ['Node.js']);
     await runSaga({ dispatch }, getTechs).toPromise();
 
     expect(dispatch).toHaveBeenCalledWith(getTechsSuccess(['Node.js']));
+  });
+
+  it('should fail api when api return error', async () => {
+    const dispatch = jest.fn();
+
+    apiMock.onGet().reply(500);
+    await runSaga({ dispatch }, getTechs).toPromise();
+
+    expect(dispatch).toHaveBeenCalledWith(getTechsFailure());
   });
 });
